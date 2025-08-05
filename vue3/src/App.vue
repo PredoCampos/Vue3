6b0966080d613477f34a1d9166840e3315ab9e7f
Pedro Campos <phenriquebcampos@gmail.com>
2025-08-05 09:25:11 -0300

    Instalação

---
+++ vue3/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<PrimeiroComponente/>
+</template>
+
+<script>
+
+import PrimeiroComponente from './components/primeirocomponente.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      PrimeiroComponente
+    }
+  }
+</script>
---
+++ vue3/src/components/primeirocomponente.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>ola :v ( °-°)</h1>
+</template>
+
+<script>
+
+  export default {
+    name: 'PrimeiroComponente'
+  }
+</script>
